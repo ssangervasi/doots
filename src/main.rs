0523ed78b8b9a5ab3e7795f8f35f6c7438668afc
Sebastian Sangervasi <villain@harmless.dev>
2020-08-07 13:45:38 -0700

    input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use std::io;
+
 use clap::App; 
 
 fn main() { 
     App::new("doots")
-       .get_matches(); 
+       .get_matches();
+	
+	println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);    
 }
