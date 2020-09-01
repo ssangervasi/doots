23105443258f0572579b85bbd172ff1fb5ce8cd3
Sebastian Sangervasi <villain@harmless.dev>
2020-08-31 18:19:38 -0700

    oh no it is a genius

--- src/ai/doot.rs
+++ src/ai/doot.rs
@@ -1,4 +1,4 @@
-use crate::game::board::{edge, Board, Edge};
+use crate::game::board::{Board, Edge};
 use crate::players::player::{Player, PlayerId};
 
 pub const KEY: &str = "doot";
@@ -23,20 +23,35 @@ impl Player for AI {
     }
 
     fn play(&self, board: Board) -> Edge {
-        let mut last_free_edge = edge((0, 0), (0, 1));
+        let mut scored_edges: Vec<(i8, Edge)> = vec![];
+
         for edge in board.iter_edges() {
             if !board.is_free(edge) {
                 continue;
             }
-            last_free_edge = edge;
+            let mut score: i8 = 1;
 
             // If a box can be taken, return immediately.
             if board.would_claim_box(edge) {
-                return edge;
+                score += 2
             }
+
+            // Check if drawing the edge would open up a box for the opponent.
+            let mut hypothetical_board = board.clone();
+            hypothetical_board
+                .draw((self.id, edge))
+                .expect("Invalid draw");
+            let gives_to_opponent = hypothetical_board
+                .iter_edges()
+                .any(|opponent_edge| hypothetical_board.would_claim_box(opponent_edge));
+            if gives_to_opponent {
+                score -= 1
+            }
+
+            scored_edges.push((score, edge));
         }
 
-        // If no squares were found, just dumly take a free edge.
-        last_free_edge
+        scored_edges.sort_by_key(|&(s, _)| s);
+        scored_edges.last().unwrap().1
     }
 }
